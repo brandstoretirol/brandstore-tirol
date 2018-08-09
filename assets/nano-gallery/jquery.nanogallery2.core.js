fd6c23a63def6c360850386ca48d1f68ac24bee7
Tom <socialmedia@brandstore.tirol>
2018-08-09 15:46:30 +0200

    nGY2paginationRectangle

--- assets/nano-gallery/jquery.nanogallery2.core.js
+++ assets/nano-gallery/jquery.nanogallery2.core.js
@@ -2986,14 +2986,7 @@
           paginationNextPage();
         });
       }
-      //display next rect
-      if( G.O.galleryPaginationMode == 'RECTANGLES' && G.GOM.pagination.currentPage >= 0 ) {
-        var $eltNext = jQuery('<div class="nGY2PaginationPrev">' + G.O.icons.paginationPrevious + '</div>').appendTo(G.$E.conTnParent);
-        $eltNext.click( function(e) {
-          paginationNextPage();
-        });
-      }
-
+      
       G.$E.conTnBottom.css('opacity', 1);
 
     }
