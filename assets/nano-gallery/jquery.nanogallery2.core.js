a675970354dcf33c34b4648e58a559ed80859aef
Tom <socialmedia@brandstore.tirol>
2018-08-11 09:46:14 +0200

    viewer

--- assets/nano-gallery/jquery.nanogallery2.core.js
+++ assets/nano-gallery/jquery.nanogallery2.core.js
@@ -1319,7 +1319,7 @@
     
     viewer :                      'internal',
     viewerFullscreen:             false,
-    viewerDisplayLogo :           true,
+    viewerDisplayLogo :           false,
     imageTransition :             'swipe',
     viewerTransitionMediaKind :   'img',
     viewerZoom :                  true,
